dcac79064726b03e1cc20ed4a6465d79a170f453
brightvvater <hscho199463@gmail.com>
2023-11-14 16:36:00 +0900

    feat: 알림URL수정

--- src/main/java/bitedu/bipa/simplesignalarm/config/WebConfig.java
+++ src/main/java/bitedu/bipa/simplesignalarm/config/WebConfig.java
@@ -23,7 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 엔드포인트에 대한 CORS 설정을 적용
-                .allowedOrigins("http://localhost:3000","http://localhost:8080", "https://simple-sign-frontend-orpin.vercel.app")
+                .allowedOrigins("http://localhost:3000","http://localhost:8080", "https://ec2-43-202-224-51.ap-northeast-2.compute.amazonaws.com", "https://simple-sign-frontend-orpin.vercel.app")
                 .allowedMethods("GET", "POST", "PATCH", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowCredentials(true) // 자격 증명 허용 여부
                 .maxAge(3600); // 사전 검사 (Preflight) 요청 캐시 시간 (초)
